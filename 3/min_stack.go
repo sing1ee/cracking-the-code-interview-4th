aa1ca78e69fc3fd7461284b221cc72ebcf7d96c1
sing1ee <zh.milo@gmail.com>
2012-10-12 21:56:18 +0800

    complete the min_stack

--- 3/min_stack.go
+++ 3/min_stack.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"container/list"
 )
 
@@ -12,17 +11,14 @@ import (
 */
 
 type MinStack struct {
-	top int
-	value []int
+	value *list.List
 	minValues *list.List
 }
 
 
 func (stack *MinStack) Push(value int) {
-	if stack.top >= len(stack.value)
-	stack.value[stack.top] = value
-	stack.top++
-	if stack.min == 0 {
+	stack.value.PushBack(value)
+	if stack.minValues.Len() == 0 {
 		stack.minValues.PushBack(value)
 	} else {
 		if value < stack.minValues.Back().Value.(int) {
@@ -32,16 +28,17 @@ func (stack *MinStack) Push(value int) {
 }
 
 func (stack *MinStack) Pop() int {
-	value := stack.value[stack.top - 1]
-	stack.top--
-	if value == stack.minValues.Back().Value.(int) {
+	value := stack.value.Back();
+	ret := value.Value.(int)
+	stack.value.Remove(value)
+	if ret == stack.minValues.Back().Value.(int) {
 		stack.minValues.Remove(stack.minValues.Back())
 	}
-	return value
+	return ret
 }
 
 func (stack *MinStack) Peek() int {
-	return stack.value[stack.top - 1]
+	return stack.value.Back().Value.(int)
 }
 
 func (stack *MinStack) Min() int {
@@ -49,8 +46,18 @@ func (stack *MinStack) Min() int {
 }
 
 func (stack *MinStack) IsEmpty() bool {
-	return 0 == stack.top
+	return 0 == stack.value.Len()
 }
 
 func main() {
+	stack := &MinStack{list.New(), list.New()}
+	for i := 100; i >= 0; i-- {
+		stack.Push(i)
+	}
+	fmt.Println(stack.Min())
+	fmt.Println(stack.Pop())
+	fmt.Println(stack.Min())
 }
+
+
+
