c37b469f7da9d32ece8aa281336442665ec9f4ef
sing1ee <zh.milo@gmail.com>
2012-10-30 17:16:51 +0800

    a * a - b * b = (a + b)*(a - b)

--- 20/add_no_oper.go
+++ 20/add_no_oper.go
@@ -13,8 +13,12 @@ func AddNoOper(a, b int) int {
 	return AddNoOper(c, carry)
 }
 
+func AddNoOper2(a, b int) int {
+	return (a * a - b * b) / (a - b)
+}
+
 func main() {
 	var a, b int
 	fmt.Scanf("%d%d", &a, &b)
-	fmt.Println(AddNoOper(a, b))
+	fmt.Println(AddNoOper(a, b), AddNoOper2(a, b))
 }
